0c179b61b276c4535daf87a917d50a4b9a6c5ed4
davidmikolasek <davidmikolasek9@gmail.com>
2025-01-13 10:58:52 +0100

    comnit

--- src/test/java/org/example/MyMinTest.java
+++ src/test/java/org/example/MyMinTest.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyMinTest {
-
+@Test
+    public void testjedna() {
+    int x = 4;
+    int y = 8;
+    assertEquals(MyMin.min(x,y), 4);
+}
 }
